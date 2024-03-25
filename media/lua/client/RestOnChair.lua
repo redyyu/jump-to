5bc4e7b930c18e8a62595d0d949d471b8cdca6cb
redyyu <redy.ru@gmail.com>
2024-03-25 17:02:29 +0800

    xxx

--- media/lua/client/RestOnChair.lua
+++ media/lua/client/RestOnChair.lua
@@ -13,10 +13,33 @@ Rch.CHAIR_NAMES = {
     ["Seat"] = true,
 }
 
+Rch.getSitChairSquare = function(chair)
+    local sprite = chair:getSprite()
+    local chairSquare = chair:getSquare()
+    local sitSquare = nil
+    if sprite and sprite:getProperties() then
+        local props = sprite:getProperties()
+        local facing = props:Val("Facing")
+        if facing == "S" then
+            sitSquare = chairSquare:getAdjacentSquare(IsoDirections.S)
+        elseif facing == "E" then
+            sitSquare = chairSquare:getAdjacentSquare(IsoDirections.E)
+        elseif facing == "W" then
+            sitSquare = chairSquare:getAdjacentSquare(IsoDirections.W)
+        elseif facing == "N" then
+            sitSquare = chairSquare:getAdjacentSquare(IsoDirections.N)
+        end
+    end
+    return sitSquare
+end
 
 Rch.onSitChair = function(chair, playerObj)
-    if luautils.walkAdj(playerObj, chair:getSquare()) then
-        ISTimedActionQueue.add(ISRestOnChairAction:new(playerObj))
+    if chair and playerObj then
+        local square = Rch.getSitChairSquare(chair)
+        if square then
+            ISTimedActionQueue.add(ISWalkToTimedAction:new(playerObj, square))
+        end
+        ISTimedActionQueue.add(ISRestOnChairAction:new(playerObj, chair))
     end
 end
 
@@ -36,9 +59,24 @@ Rch.onFillWorldObjectContextMenu = function(playerNum, context, worldobjects)
         end
     end
     local restOpt = context:getOptionFromName(getText("ContextMenu_Rest"))
-    if chair and restOpt then
-        context:insertOptionBefore(restOpt.name, getText("ContextMenu_Rest_Chair"), chair, Rch.onSitChair, playerObj)
-        context:removeOptionByName(restOpt.name)
+    if chair then
+        if restOpt then
+            context:removeOptionByName(restOpt.name)
+        end 
+        local option = context:addOptionOnTop(getText("ContextMenu_Rest_Chair"), chair, Rch.onSitChair, playerObj)
+        option.notAvailable = playerObj:getStats():getEndurance() >= 1
+        option.toolTip = ISWorldObjectContextMenu.addToolTip()
+        option.toolTip:setName(getText("Tooltip_Rest_On_Chair"))
+
+        local bedType = chair:getProperties():Val("BedType") or "averageBed";
+        local bedTypeXln = getTextOrNull("Tooltip_BedType_" .. bedType)
+        if bedTypeXln then
+            option.toolTip.description = getText("Tooltip_BedType", bedTypeXln)
+        end
+        
+        if option.notAvailable then
+            option.toolTip.description = '<RGB:1,0,0> ' .. getText("Tooltip_Rest_NotTiredEnough") ..' <RGB:1,1,1> <BR>'.. option.toolTip.description
+        end
     end
 end
 
--- media/lua/client/TimedActions/ISRestOnChairAction.lua
+++ media/lua/client/TimedActions/ISRestOnChairAction.lua
@@ -4,7 +4,27 @@ require "TimedActions/ISBaseTimedAction"
 ISRestOnChairAction = ISBaseTimedAction:derive("ISRestOnChairAction")
 
 function ISRestOnChairAction:isValid()
-	return self.character:getStats():getEndurance() < 1
+	return self.character:getStats():getEndurance() < 1 and 
+		   self.chair:getSprite() ~= nil and 
+		   self.chair:getSprite():getProperties() ~= nil
+end
+
+function ISRestOnChairAction:waitToStart()
+	local facingX = self.character:getCurrentSquare():getX()  -- make sure X, Y not middle of square.
+	local facingY = self.character:getCurrentSquare():getY()
+	local props = self.chair:getSprite():getProperties()
+	local facing = props:Val("Facing")
+	if facing == "S" then
+		facingY = facingY + 10
+	elseif facing == "E" then
+		facingX = facingX - 10
+	elseif facing == "W" then
+		facingX = facingX + 10
+	elseif facing == "N" then
+		facingY = facingY - 10
+	end
+    self.character:faceLocation(facingX, facingY)
+	return self.character:shouldBeTurning()
 end
 
 function ISRestOnChairAction:update()
@@ -27,11 +47,12 @@ function ISRestOnChairAction:perform()
 	ISBaseTimedAction.perform(self)
 end
 
-function ISRestOnChairAction:new(character)
+function ISRestOnChairAction:new(character, chair)
 	local o = {}
 	setmetatable(o, self)
 	self.__index = self
 	o.character = character
+	o.chair = chair
 	o.stopOnWalk = true
 	o.stopOnRun = true
     o.mul = 2
